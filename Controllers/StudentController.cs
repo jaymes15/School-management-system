8a37b317ba4d420595f69468dd6b6b4799bba9ab
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2020-04-21 23:26:14 -0700

    GetAllStudent method StudentController

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WEBWORK.DATA.DataContext;
+
+namespace WEBWORK.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : Controller 
+    { 
+
+
+        private ApplicationDbContext _context;
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetAllStudent")]
+        [Produces("application/json")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+
+        public IActionResult GetAllStudent()
+        {
+            return Ok(_context.Students.ToList());
+        }
+
+
+    }
+}
