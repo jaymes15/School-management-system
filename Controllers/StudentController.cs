b2bd88882a623a6e8032ef11b51cc1125969a64f
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2020-04-22 00:07:49 -0700

    UpdateStudent method StudentController

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -20,6 +20,8 @@ namespace WEBWORK.Controllers
             _context = context;
         }
 
+
+        #region Get All Student
         [HttpGet(Name = "GetAllStudent")]
         [Produces("application/json")]
         [ProducesResponseType(200)]
@@ -29,7 +31,9 @@ namespace WEBWORK.Controllers
         {
             return Ok(_context.Students.ToList());
         }
+        #endregion
 
+        #region Get Student By Id
         [HttpGet("{id}",Name = "GetStudentById")]
         [Produces("application/json")]
         [ProducesResponseType(200)]
@@ -48,7 +52,9 @@ namespace WEBWORK.Controllers
             }
 
         }
+        #endregion
 
+        #region Create Student
         [HttpPost( Name = "CreateStudent")]
         [Produces("application/json")]
         [ProducesResponseType(200)]
@@ -69,6 +75,41 @@ namespace WEBWORK.Controllers
             }
             
         }
+#endregion
+
+        #region Update Student
+        [HttpPut("{id}",Name = "UpdateStudent")]
+        [Produces("application/json")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+     
+        public IActionResult UpdateStudent([FromRoute] int id,[FromBody] StudentData studentData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else 
+            {
+                var student = _context.Students.Find(id);
+                if (student == null)
+                {
+                    return NotFound(ModelState);
+                }
+                else
+                {
+                    student.FirstName = studentData.FirstName;
+                    student.LastName = studentData.LastName;
+                    student.Email = studentData.Email;
+                    student.Phone = studentData.Phone;
+                    _context.SaveChanges();
+                    return Ok(student);
+                }
+                
+            }
+
+        }
+        #endregion
 
 
 
