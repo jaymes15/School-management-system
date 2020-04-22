9c030eb9ba8ab7b088a5f5d6606e38be3aa1289b
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2020-04-21 22:53:08 -0700

    Application Configuration and DB setup

---
+++ Configurations/ServicesConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEBWORK.DATA.DataContext;
+
+namespace WEBWORK.Configurations
+{
+    public static class ServicesConfiguration
+    {
+        public static IServiceCollection AddDbConnection(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options
+                .UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("WEBWORK")));
+
+            return services;
+        }
+    }
+}
